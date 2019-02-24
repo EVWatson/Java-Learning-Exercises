9ffaf57fb2ba541e4a96227891427adbdabd5f6b
EVWatson <emily.watson@myob.com>
2019-02-25 08:29:35 +1100

    codealong from java workshop

--- friday_java_workshop_codealong/test/MainTest.java
+++ friday_java_workshop_codealong/test/MainTest.java
@@ -1,2 +1,16 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class MainTest {
+    @Test
+
+    public void canReverseAsString(){
+        String input = "Friday";
+        String expected = "yadirF";
+        Main m = new Main();
+        String actual = m.reverse(input);
+
+        assertEquals(actual, expected);
+    }
 }
