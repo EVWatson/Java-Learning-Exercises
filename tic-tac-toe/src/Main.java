115c28273de4832c7aa829467cedcd8498391a0c
EVWatson <emily.watson@myob.com>
2019-02-26 14:57:10 +1100

    more experiments with 2d  arraylist; started game board print -out

--- tic-tac-toe/src/Main.java
+++ tic-tac-toe/src/Main.java
@@ -1,13 +1,22 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
 
 
+    public static void main(String[] args) {
 
+        String[][] theGameBoard = new String[3][3];
+//            theGameBoard[1][2] = "hello";
+        for (int x = 0; x <= 2; x++) {
+            for (int y = 0; y <= 2; y++) {
+                theGameBoard[x][y] = "*";
+                System.out.print(theGameBoard[x][y] + "    ");
+            }
+            System.out.println(" \n");
+//            System.out.println();
 
+        }
+    }
 
 }
 
