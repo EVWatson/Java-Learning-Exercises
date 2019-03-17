e87cc010bf06f305fe6138926418fe84b4de2f6b
EVWatson <emily.watson@myob.com>
2019-03-18 09:38:10 +1100

    started refactoring fizzbuzz with classes

---
+++ fizz_buzz_kata/src/Fizz.java
@@ -0,0 +1,11 @@
+public class Fizz {
+
+    private static boolean isNumberMultipleOf3 (int number) {
+        return (number % 3 == 0);
+    }
+
+    public static String getIsNumberMultipleOf3(){
+        return isNumberMultipleOf3(number);
+    }
+
+}
